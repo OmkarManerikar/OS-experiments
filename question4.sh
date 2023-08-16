da880fa646f78ea9b4ef820e4f008a014ea34d0f
Omkar Manerikar <114423628+OmkarManerikar@users.noreply.github.com>
2023-08-16 12:54:35 +0530

    Add files via upload

--- question4.sh
+++ question4.sh
@@ -1,3 +1,14 @@
-read a[5]
-echo ${a[0]}
+echo "enter the character whose sequence u want to check"
+read a
 
+sp=' '
+count=0
+read x
+for((i=0;i<${#x};i++))
+do
+	if [ "${x:i:1}" == "$a" ] 
+	then
+		count=$(($count+1))
+	fi
+done
+echo $count
